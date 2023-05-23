29ad87d2fc4f6e22e745764b9bf8483fa45f9d0e
Claudio <c.s.r.m@hotmail.com>
2023-05-23 13:12:53 -0500

    Se agrego la funcion de que el texto que se escribe se copie en el texto donde estara la parte encriptada

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ function llamar(ID){
 }
 //Variables del html para js
 var encriptar = llamar("mensaje"); //poner lo que entra en el text area en la vaible encriptar
-var verificador = false; 
 var botonencriptador = llamar("encriptar");
 var avisos = llamar("avisos");
 var copiar = llamar("copiar")
 var desencriptar = llamar("desencriptar")
+var textoDesencriptado= llamar('mensaje-desencriptado')
+var verificador = false; 
 
 //Para que el textarea solo admita minusculas 
 function nopermitidos(texto){
     let nuevo = texto.toLowerCase(); //convierte todo el texto del textarea en minusculas
-    const verificar = /[^a-z\s]/gi; //aca solo estan de la a-z
+    let verificar = /[^a-zñ\s]/gi; //aca solo estan de la a-z
     if(nuevo!= 0){
         verificador= true;
     }
     return nuevo.replace(verificar, ""); //el comando replace... hace que primero verifique el texto dentro de la variable "nuevo" con verificar para ver si coincide.... si no coincide entonces lo reemplza por un espacio.
 }
-
 
 encriptar.addEventListener('input',function(){ //realiza un evento
     var texto = encriptar.value;
@@ -26,15 +26,19 @@ encriptar.addEventListener('input',function(){ //realiza un evento
     if (encriptar.value==""){
         avisos.style.display="inline-block";
         copiar.style.display="none";
+        return textoDesencriptado.value = texto;
     }
 });
 
 
 botonencriptador.addEventListener('click', function(){
+    let texto1 = encriptar.value;
+    let texto2 = "  "+texto1
     if (verificador){
                 avisos.style.display="none";
                 copiar.style.display="inline-block";
     }
+    return textoDesencriptado.value = texto2;
 });
 
 desencriptar.addEventListener('click',function(){
@@ -42,4 +46,8 @@ desencriptar.addEventListener('click',function(){
         avisos.style.display="none";
         copiar.style.display="inline-block";
     }
+    let texto1 = encriptar.value;
+    let texto2 = "  "+texto1
+    return textoDesencriptado.value = texto2;
 })
+
